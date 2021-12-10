8ed108cc6553a6a5515c6fcc9bf94a9830ccf3ab
Connor Burns <ctrekker4@gmail.com>
2021-12-09 17:32:27 -0700

    Tableaux proof method

--- src/PropositionalLogic.jl
+++ src/PropositionalLogic.jl
@@ -4,11 +4,11 @@ using Symbolics
 using Symbolics: Sym, Symbolic, Term
 using DataFrames
 
-export LogicalSymbol, truthtable
+export LogicalSymbol, truthtable, prove
 export ¬, →, ⟶, ←, ⟵, ↔, ⟷, ∨, ∧
 
-SB = Union{Symbolic{Bool}, Term}
 LogicalSymbol = Sym{Bool}
+SB = Union{LogicalSymbol, Term}
 
 ∨(p::Bool, q::Bool) = p || q
 ∨(p::SB, q::SB) = Term(∨, [p, q])
@@ -31,7 +31,7 @@ LogicalSymbol = Sym{Bool}
 ↔ = ⟷
 
 
-function truthtable(st::Term)
+function truthtable(st::SB)
     variables = Symbolics.get_variables(st)
     combinations = 2 ^ length(variables)
     st_sym = Symbol(string(st))
@@ -44,6 +44,75 @@ function truthtable(st::Term)
     end
 
     table
+end
+
+
+demorgan_and = @rule ¬(~p ∧ ~q) => ¬~p ∨ ¬~q
+demorgan_or = @rule ¬(~p ∨ ~q) => ¬~p ∧ ¬~q
+double_negative = @rule ¬¬~p => ~p
+material_implication = @rule ~p → ~q => ¬~p ∨ ~q
+reverse_material_implication = @rule ~p ← ~q => ~p ∨ ¬~q
+negated_material_implication = @rule ¬(~p → ~q) => ~p ∧ ¬~q
+negated_reverse_material_implication = @rule ¬(~p ← ~q) => ¬~p ∧ ~q
+material_equivalence = @rule ~p ⟷ ~q => (~p ∧ ~q) ∨ (¬~p ∧ ¬~q)
+negated_material_equivalence = @rule ¬(~p ⟷ ~q) => (~p ∧ ¬~q) ∨ (¬~p ∧ ~q)
+
+simplify_statement = Symbolics.RestartedChain([
+    demorgan_and,
+    demorgan_or,
+    double_negative,
+    material_implication,
+    reverse_material_implication,
+    negated_material_implication,
+    negated_reverse_material_implication,
+    material_equivalence,
+    negated_material_equivalence
+])
+
+function prove(proposition::SB)
+    prove(Set{SB}([proposition]))
+end
+function prove(propositions::Union{Set, Vector})
+    simplified_propositions = Set(simplify.(propositions; rewriter=simplify_statement))
+    return _prove_simplified(simplified_propositions)
+end
+function _prove_simplified(propositions::Set)
+    @info propositions
+    for p ∈ propositions
+        pv = Symbolics.value(p)
+
+        if pv isa LogicalSymbol
+            # check for negation (contradiction)
+            if any([isequal(¬p, q) for q ∈ propositions])
+                return false
+            end
+        else
+            term_op = operation(pv)
+            term_args = arguments(pv)
+            reduced_propositions = setdiff(propositions, Set([p]))
+            if length(term_args) == 2 # binary operation
+                if term_op == ∧
+                    # break up term and make one recursive call
+                    if !prove(reduced_propositions ∪ Set(term_args))
+                        return false
+                    end
+                elseif term_op == ∨
+                    # consider both arguments of term and make two recursive calls
+                    if !prove(reduced_propositions ∪ Set([term_args[1]])) && !prove(reduced_propositions ∪ Set([term_args[2]]))
+                        return false
+                    end
+                end
+            else # unary operation (¬)
+                term_arg = first(term_args)
+                # if the argument is a term, throw an error. this should never happen since it was removed beforehand
+                if term_arg isa Term
+                    throw(ErrorException("Improperly expanded predicate!"))
+                end
+            end
+        end
+    end
+
+    return true
 end
 
 _dict_from_combination_index(variables, index::Int) = Dict([variables[i] => Bool(index >> (i-1) & 1) for i ∈ 1:length(variables)])
